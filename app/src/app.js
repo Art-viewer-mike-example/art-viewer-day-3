4c41513f6ee3d297772cd4bbbb87fd6307d50bb7
Mike Cronin <mostlyfocusedmike@gmail.com>
2023-08-26 22:30:12 -0400

    [ART-5] Add form to page for search (non functional) (#3)
    * [ART-5] Clean out console
    
    * [ART-5] render form

--- app/src/app.js
+++ app/src/app.js
@@ -1,6 +1,5 @@
 import {
   fetchAllArtByKeyword,
-  fetchArtworkById
 } from "./fetch-funcs";
 import {
   mainSetup,
@@ -10,14 +9,6 @@ import {
 export default async function app(mainEl) {
   const { paintingsContainer } = mainSetup(mainEl);
 
-  // TODO: remove this code when we fetch for real
   const artworks = await fetchAllArtByKeyword()
-  console.log('Fetch #1, list')
-  console.table(artworks)
-
-  const artwork = await fetchArtworkById(artworks[0].id)
-  console.log('Fetch #2, single')
-  console.log(artwork)
-
   renderPaintings(paintingsContainer, artworks)
 }
